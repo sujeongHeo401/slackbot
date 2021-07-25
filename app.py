212e2b5fc242c48b2ff1cacb761cdbd0e0dd3487
sujeongHeo401 <86057499+sujeongHeo401@users.noreply.github.com>
2021-07-25 14:28:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ slack_web_client = WebClient(token=os.environ.get("SLACKBOT_TOKEN"))
 
 slack_events_adapter = SlackEventAdapter(os.environ.get("SLACK_EVENTS_TOKEN"), "/slack/events", app)
 
-MESSAGE_BLCK = {
-    "type" : "section",
-    "text" : {
-        "type" : "mrkdwn", 
-        "text" : "",
-    }
+MESSAGE_BLOCK = {
+    "type": "section",
+    "text": {
+        "type": "mrkdwn",
+        "text": "",
+    },
 }
 
 @slack_events_adapter.on("message")
@@ -28,16 +28,16 @@ def message(payload):
     if "flip a coin" in text.lower():
         channel_id = event.get("channel")
 
-        rand_int = random.randomint(0, 1)
+        rand_int = random.randint(0, 1)
         if rand_int == 0:
             results = "Heads"
         else:
             results = "Tails"
-        
         message = f"The result is {results}"
 
-        MESSAGE_BLCK["text"]["text"] = message
-        x = {"channel" : channel_id, "blocks" : [MESSAGE_BLCK]}
+        MESSAGE_BLOCK["text"]["text"] = message
+
+        x = {"channel": channel_id, "blocks": [MESSAGE_BLOCK]}
 
         return slack_web_client.chat_postMessage(**x)
 
